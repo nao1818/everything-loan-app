6d586f33670b3fb8878a987e0c42f673bc1d374c
nao <nao@nao-no-MacBook-Pro.local>
2016-10-05 18:15:29 +0900

    first commit

---
+++ app/Http/Controllers/AdminAuth/PasswordController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\AdminAuth;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
+
+    use ResetsPasswords;
+
+
+    protected $redirectTo = '/admin/home';
+    protected $linkRequestView ='adminAuth.passwords.email';
+    protected $resetView = 'adminAuth.passwords.reset';
+    protected $guard = 'admin';
+    protected $broker = 'admins';
+
+    /**
+     * Create a new password controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin');
+    }
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::auth();
+
+// Authentication Routes...
+$this->get('admin/login', 'AdminAuth\AuthController@showLoginForm');
+$this->post('admin/login', 'AdminAuth\AuthController@login');
+$this->get('admin/logout', 'AdminAuth\AuthController@logout');
+
+// Registration Routes...
+$this->get('admin/register', 'AdminAuth\AuthController@showRegistrationForm');
+$this->post('admin/register', 'AdminAuth\AuthController@register');
+
+// Password Reset Routes...
+$this->get('admin/password/reset/{token?}', 'AdminAuth\PasswordController@showResetForm');
+$this->post('admin/password/email', 'AdminAuth\PasswordController@sendResetLinkEmail');
+$this->post('admin/password/reset', 'AdminAuth\PasswordController@reset');
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('admin/home', 'AdminHomeController@index');
+
+
